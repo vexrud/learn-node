d6638bde48662bd36ee7f951bdc6a86dacf8bafa
Umut Batuhan ALPAN <batuhan@Mac.hgw.local>
2025-09-25 20:02:45 +0300

    add mongoose library and database connection

---
+++ api/db/Database.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+let instance = null;
+class Database {
+
+    constructor(){
+        if (!instance) {
+            this.mongoConnection = null;
+            instance = this;
+        }
+
+        return instance;
+    }
+
+    async connect(options){
+        try{
+            console.log("DB connecting...");
+            let db = await mongoose.connect(options.CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
+
+            this.mongoConnection = db;
+            console.log("DB connected.");
+        }
+        catch (err) {
+            console.error(err);
+            process.exit(1);
+        }
+    }
+}
+
+module.exports = Database;
